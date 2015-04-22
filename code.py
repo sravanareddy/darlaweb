ef21a4a49b54f7c4fc39d7514fc48402ba511cfb
sravanareddy <sravana@cs.dartmouth.edu>
2015-04-22 03:09:31 -0400

    Reads and validates form input, creates task and stores updloaded file. Form validation when file is uploaded is buggy, and radio validation not implemented.

--- code.py
+++ code.py
@@ -4,56 +4,73 @@ import web
 import shutil
 from web import form
 import myform
+import utilities
+import os
 
 render = web.template.render('templates/', base='layout')
 
 urls = ('/', 'index', '/upload', 'upload')
 app = web.application(urls, globals())
 web.config.debug = True
-    
-uploadfile = form.File('uploadfile',
-                       post='Longer recordings (of at least 30 minutes) are recommended. Your uploaded files are stored temporarily on the Dartmouth servers in order to process your job, and deleted after.',
-                       description='Upload a .wav, .mp3, or .zip file with multiple recordings',)
-
-dialect = form.Radio('dialect',
-                     [('standard', 'Standard American '),
-                      ('southern', 'Southern ')],
-                     post='Selecting the appropriate dialect for the acoustic model may increase transcription accuracy. If your data contains speakers of multiple dialects, select Standard American. Other dialects may be added in the future.',
-                     description='Dialect of the majority of speakers')
-
-lw = form.Radio('lw',
-               [(7, 'Free speech or reading passage '),
-                (3, 'Word list ')],
-                post='If your recording contains both styles, select the free speech option.',
-                description='Speech Type',)
-
-email = form.Textbox('email',
-                     form.regexp(r"^[\w.+-]+@[\w.+-]+\.[\w.+-]+$", 
-                                 "Please enter a valid email address"),
-                     post='We will not store or distribute your address.',
-                     description='Your e-mail address')
-
-uploadsound = myform.MyForm(uploadfile, dialect, lw, email)
-    
+        
 class index:
     def GET(self):
         return render.index()
 
-class upload: 
-    def GET(self): 
-        form = uploadsound()
-        # make sure you create a copy of the form by calling it (line above)
-        # Otherwise changes will appear globally
+class upload:
+    uploadfile = form.File('uploadfile',
+                           post='Longer recordings (of at least 30 minutes) are recommended. Your uploaded files are stored temporarily on the Dartmouth servers in order to process your job, and deleted after.',
+                           description='Upload a .wav, .mp3, or .zip file with multiple recordings')
+    filelink = form.Textbox('filelink',
+                            form.regexp(r'^$|https\://www\.youtube\.com/watch\?v\=\S+', 'Check your link. It should start with https://www.youtube.com/watch?v='),
+                              post='Long, single-speaker videos with no music work best',
+                              description='or copy and paste a link to a YouTube video')
+    dialect = form.Radio('dialect',
+                         [('standard', 'Standard American '),
+                          ('southern', 'Southern ')],
+                         post='Selecting the appropriate dialect for the acoustic model may increase transcription accuracy. If your data contains speakers of multiple dialects, select Standard American. Other dialects may be added in the future.',
+                         description='Dialect of the majority of speakers')
+    lw = form.Radio('lw',
+                    [(7, 'Free speech or reading passage '),
+                     (3, 'Word list ')],
+                    post='If your recording contains both styles, select the free speech option.',
+                    description='Speech Type',)
+    email = form.Textbox('email',
+                         form.notnull,
+                         form.regexp(r'^[\w.+-]+@[\w.+-]+\.[\w.+-]+$',
+                                     'Please enter a valid email address.'),
+                         post='We will not store or distribute your address.',
+                         description='Your e-mail address')
+    uploadsound = myform.MyForm(uploadfile, 
+                                filelink, 
+                                dialect, 
+                                lw, 
+                                email, 
+                                validators=[form.Validator('Please upload a file or enter a video link.', lambda x: x.uploadfile or x.filelink), 
+                                            form.Validator('Upload a file OR select a YouTube video, not both.', lambda x: not (x.uploadfile and x.filelink))])
+    
+    datadir = '/home/sravana/data/webphonetics/'
+    
+    def GET(self):
+        form = self.uploadsound()
         return render.formtest(form)
 
     def POST(self): 
-        form = uploadsound() 
-        if not form.validates(): 
+        form = self.uploadsound()
+        x = web.input(uploadfile={})
+        if not form.validates(): #TODO: this crashes if a file was uploaded but the rest of the form didn't validate. seems to be a problem with "rendering" file inputs.
             return render.formtest(form)
         else:
-            # form.d.boe and form['boe'].value are equivalent ways of
-            # extracting the validated arguments from the form.
-            return render.formtest("Grreat success! email: {0}".format(form['email'].value))
+            #create new task                                                               
+            taskname = utilities.make_task(self.datadir)
+            #sanitize filename
+            filename = utilities.get_basename(x.uploadfile.filename)
+            #write contents of file
+            o = open(os.path.join(self.datadir, taskname+'.wav', filename), 'w')
+            o.write(x.uploadfile.file.read())
+            o.close()
+            
+            return "Great success! your file: {0}, email: {1}".format(filename, form.email.value)
 
 if __name__=="__main__":
     web.internalerror = web.debugerror
