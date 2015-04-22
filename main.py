79d5e1cee5afad5dad3a2fef7ce12e386e67430a
sravanareddy <sravana@cs.dartmouth.edu>
2015-04-22 17:24:55 -0400

    various changes

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class index:
         return render.index()
 
 class upload:
-    uploadfile = form.File('uploadfile',
+    uploadfile = myform.MyFile('uploadfile',
                            post='Longer recordings (of at least 30 minutes) are recommended. Your uploaded files are stored temporarily on the Dartmouth servers in order to process your job, and deleted after.',
                            description='Upload a .wav, .mp3, or .zip file with multiple recordings')
     filelink = form.Textbox('filelink',
@@ -28,11 +28,13 @@ class upload:
     dialect = form.Radio('dialect',
                          [('standard', 'Standard American '),
                           ('southern', 'Southern ')],
+                         value = 'standard',
                          post='Selecting the appropriate dialect for the acoustic model may increase transcription accuracy. If your data contains speakers of multiple dialects, select Standard American. Other dialects may be added in the future.',
                          description='Dialect of the majority of speakers')
     lw = form.Radio('lw',
-                    [(7, 'Free speech or reading passage '),
-                     (3, 'Word list ')],
+                    [('7', 'Free speech or reading passage '),
+                     ('3', 'Word list ')],
+                    value = '7',
                     post='If your recording contains both styles, select the free speech option.',
                     description='Speech Type',)
     email = form.Textbox('email',
@@ -58,7 +60,7 @@ class upload:
     def POST(self): 
         form = self.uploadsound()
         x = web.input(uploadfile={})
-        if not form.validates(): #TODO: this crashes if a file was uploaded but the rest of the form didn't validate. seems to be a problem with "rendering" file inputs.
+        if not form.validates(): #TODO: can't display uploaded filename.
             return render.formtest(form)
         else:
             #create new task                                                               
@@ -69,8 +71,11 @@ class upload:
             o = open(os.path.join(self.datadir, taskname+'.wav', filename), 'w')
             o.write(x.uploadfile.file.read())
             o.close()
+        
+            #disable form
+            form.disable()
             
-            return "Great success! your file: {0}, email: {1}".format(filename, form.email.value)
+            return render.formtest(form)
 
 if __name__=="__main__":
     web.internalerror = web.debugerror
--- myform.py
+++ myform.py
@@ -1,6 +1,15 @@
 import web
 
-class MyForm(web.form.Form):
+class MyFile(web.form.Input):
+        def render(self):
+                attrs = self.attrs.copy()
+                attrs['type'] = 'file'
+                if self.value:
+                        attrs['value'] = ''
+                attrs['name'] = self.name
+                return '<input {0}/>'.format(attrs)
+
+class MyForm(web.form.Form):        
         """Modify default rendering behavior"""
         def rendernote(self, note):
                 if note: 
@@ -14,9 +23,20 @@ class MyForm(web.form.Form):
                 for i in self.inputs:
                         out.append('<p>')
                         out.append(i.description+': ')
-                        out.append(i.render()) 
+                        out.append(i.render())
                         out.append(self.rendernote(i.note))
                         out.append('<br><span class="note">{0}</span>'.format(i.post))
                         out.append('</p>\n')
                 return ''.join(out) 
 
+        def render_disabled(self):
+                out = []
+                out.append(self.rendernote(self.note))
+                for i in self.inputs:
+                        out.append('<p>')
+                        out.append(i.description+': ')
+                        out.append(i.render())
+                        out.append(self.rendernote(i.note))
+                        out.append('<br><span class="note">dis{0}</span>'.format(i.post))
+                        out.append('</p>\n')
+                return ''.join(out)
