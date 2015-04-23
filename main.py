c50d6a3ec0c3bce2471d21770cdcee5e65fa2368
Sravana Reddy <sravana@cs.dartmouth.edu>
2015-04-23 15:30:15 -0400

    file upload and taskname creation

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class index:
         return render.index()
 
 class upload:
-    uploadfile = form.File('uploadfile',
+    uploadfile = myform.MyFile('uploadfile',
                            post='Longer recordings (of at least 30 minutes) are recommended. Your uploaded files are stored temporarily on the Dartmouth servers in order to process your job, and deleted after.',
                            description='Upload a .wav, .mp3, or .zip file with multiple recordings')
     filelink = form.Textbox('filelink',
@@ -31,8 +31,8 @@ class upload:
                          post='Selecting the appropriate dialect for the acoustic model may increase transcription accuracy. If your data contains speakers of multiple dialects, select Standard American. Other dialects may be added in the future.',
                          description='Dialect of the majority of speakers')
     lw = form.Radio('lw',
-                    [(7, 'Free speech or reading passage '),
-                     (3, 'Word list ')],
+                    [('7', 'Free speech or reading passage '),
+                     ('3', 'Word list ')],
                     post='If your recording contains both styles, select the free speech option.',
                     description='Speech Type',)
     email = form.Textbox('email',
@@ -41,37 +41,50 @@ class upload:
                                      'Please enter a valid email address.'),
                          post='We will not store or distribute your address.',
                          description='Your e-mail address')
-    uploadsound = myform.MyForm(uploadfile, 
-                                filelink, 
-                                dialect, 
-                                lw, 
-                                email, 
-                                validators=[form.Validator('Please upload a file or enter a video link.', lambda x: x.uploadfile or x.filelink), 
-                                            form.Validator('Upload a file OR select a YouTube video, not both.', lambda x: not (x.uploadfile and x.filelink))])
+    taskname = form.Hidden('taskname')
+    
+    soundvalid = [form.Validator('Please upload a file or enter a video link (but not both).', lambda x: (x.filelink!='' or x.uploadfile) and not (x.uploadfile and x.filelink!=''))]   #TODO: why doesn't this work?
     
     datadir = open('filepaths.txt').read().strip()
     
     def GET(self):
-        form = self.uploadsound()
+        self.dialect.value = 'standard'
+        self.lw.value = '7' #defaults
+        uploadsound = myform.MyForm(self.uploadfile, 
+                                    self.filelink, 
+                                    self.dialect, 
+                                    self.lw, self.email, self.taskname)
+        form = uploadsound()
         return render.formtest(form)
 
-    def POST(self): 
-        form = self.uploadsound()
+    def POST(self):
+        uploadsound = myform.MyForm(self.uploadfile, 
+                                    self.filelink, 
+                                    self.dialect, 
+                                    self.lw, self.email, self.taskname)
+        form = uploadsound()
         x = web.input(uploadfile={})
-        if not form.validates(): #TODO: this crashes if a file was uploaded but the rest of the form didn't validate. seems to be a problem with "rendering" file inputs.
+        
+        if not form.validates(): 
             return render.formtest(form)
-        else:
+        
+        elif 'uploadfile' in x:  #TODO: handle mp3 and zip files
+            
             #create new task                                                               
             taskname = utilities.make_task(self.datadir)
+            self.taskname.value = taskname
+            
             #sanitize filename
             filename = utilities.get_basename(x.uploadfile.filename)
+            
             #write contents of file
             o = open(os.path.join(self.datadir, taskname+'.wav', filename), 'w')
             o.write(x.uploadfile.file.read())
             o.close()
             
             return "Great success! your file: {0}, email: {1}".format(filename, form.email.value)
-
+            
+    
 if __name__=="__main__":
     web.internalerror = web.debugerror
     app.run()
--- myform.py
+++ myform.py
@@ -1,5 +1,17 @@
 import web
 
+class MyFile(web.form.File):
+	"""Rendering for files should not try to display contents"""
+	def render(self):
+		attrs = self.attrs.copy()
+		attrs['type'] = self.get_type()
+		if attrs['type']=='file':
+			attrs['value'] = ''
+		else:
+			attrs['value'] = self.value
+		attrs['name'] = self.name
+		return '<input %s/>' % attrs
+		
 class MyForm(web.form.Form):
         """Modify default rendering behavior"""
         def rendernote(self, note):
@@ -12,11 +24,13 @@ class MyForm(web.form.Form):
                 out = [] 
                 out.append(self.rendernote(self.note))
                 for i in self.inputs:
-                        out.append('<p>')
-                        out.append(i.description+': ')
-                        out.append(i.render()) 
-                        out.append(self.rendernote(i.note))
-                        out.append('<br><span class="note">{0}</span>'.format(i.post))
-                        out.append('</p>\n')
+                        if not i.is_hidden():
+				out.append('<p>')
+				out.append(i.description+': ')
+                        out.append(i.render())
+                        if not i.is_hidden():
+				out.append(self.rendernote(i.note))
+				out.append('<br><span class="note">{0}</span>'.format(i.post))
+				out.append('</p>\n')
                 return ''.join(out) 
 
