aabf7855d595c274733dbb4acbadb2519654d065
irenelfeng <irene.l.feng.17@dartmouth.edu>
2015-05-28 00:08:25 -0400

    utilities error

--- utilities.py
+++ utilities.py
@@ -72,15 +72,14 @@ def send_email(receiver, filename, taskname):
         for nicename, filename in [('formants.csv', taskname+'.aggvowels_formants.csv'), ('formants.fornorm.tsv', taskname+'.fornorm.tsv'), ('plot.pdf', taskname+'.plot.pdf'), ('alignments.zip', taskname+'.alignments.zip')]:
                 part = MIMEBase('application', "octet-stream")
                 try:
-                    f = open(filename)
-                except IOError:
-                    print('error')
-                with open(filename, "rb") as result:
+                    with open(filename, "rb") as result:
 
-                    part.set_payload( open(filename,"rb").read() ) #fornorm.tsv? no file 
-                    encoders.encode_base64(part)
-                    part.add_header('Content-Disposition', 'attachment; filename='+nicename)
-                    message.attach(part)
+                        part.set_payload( open(filename,"rb").read() ) #fornorm.tsv? no file 
+                        encoders.encode_base64(part)
+                        part.add_header('Content-Disposition', 'attachment; filename='+nicename)
+                        message.attach(part)
+                except:
+                    send_error_email(receiver,filename)
             
         try:
                 server = smtplib.SMTP('smtp.gmail.com', 587)
