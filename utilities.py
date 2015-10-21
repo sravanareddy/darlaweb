324f1ca8d8f09863f697607438b3e19a1f012302
sravanareddy <sravana@cs.dartmouth.edu>
2015-10-21 04:34:04 -0400

    fixing cgi error with YT download of videos (#73)

--- utilities.py
+++ utilities.py
@@ -482,8 +482,8 @@ def process_audio(audiodir, filename, extension, filecontent, dochunk):
 def youtube_wav(url,audiodir, taskname):
     try:
         yt_command = 'youtube-dl '+url+' --extract-audio --audio-format wav --audio-quality 16k -o '+os.path.join(audiodir, 'ytvideo.%(ext)s')
-        print yt_command
-        tube = subprocess.Popen(shlex.split(yt_command), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        tube = subprocess.Popen(shlex.split(yt_command), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        tube.wait()
         return "ytvideo.wav", ""
     except:
         return "ytvideo.wav", "Could not convert youtube video to a .wav file."        
